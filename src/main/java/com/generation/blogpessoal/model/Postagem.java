ed97ee1414a0867e9f19a90dbf3b095634c53fa8
bialocatelli <beatrizglocatelli@gmail.com>
2022-04-05 23:28:37 -0300

    Criando camadas Security, Service e implementando as classes

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -34,18 +34,14 @@ public class Postagem {
 
 	@UpdateTimestamp
 	private LocalDateTime data;
-	
+
 	@ManyToOne
 	@JsonIgnoreProperties("postagem")
 	private Tema tema;
 
-	public Tema getTema() {
-		return tema;
-	}
-
-	public void setTema(Tema tema) {
-		this.tema = tema;
-	}
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
 
 	public Long getId() {
 		return id;
@@ -77,6 +73,22 @@ public class Postagem {
 
 	public void setData(LocalDateTime data) {
 		this.data = data;
+	}
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+
+	public Usuario getUsuario() {
+		return this.usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
 	}
 
 }
